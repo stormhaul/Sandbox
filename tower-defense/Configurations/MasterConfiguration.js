b7f41aa100bdbb148403c0d668975bd04dba4db1
Timothy Smith <stormhaul@github.com>
2019-01-01 23:25:58 -0600

    added master config

---
+++ tower-defense/Configurations/MasterConfiguration.js
@@ -0,0 +1,18 @@
+"using strict";
+
+var configuration = {
+  cell_width: 50,// overrides rows and columns
+  rows: 55, //overridden by cell_width
+  columns: 55, //overridden by cell_width
+  waypoints: 5,
+  min_waypoint_distance: 5,
+  grid_padding: 10,
+  canvas_id: "canvas",
+  tick_rate: 50,
+  default_gold: 400,
+  default_lives: 20,
+  tower_cost: 100,
+  tower_rate: 1,
+  tower_range: 200,
+  tower_damage: 20
+};
